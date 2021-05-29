2efbf32195349354b8594cd876352c9a6a5a58fa
md arish haque <arishhaque@mds-MacBook-Pro.local>
2021-05-30 01:15:47 +0530

    Added sql file

--- src/main/java/librarymanager/LibraryManagementApp.java
+++ src/main/java/librarymanager/LibraryManagementApp.java
@@ -1,9 +1,42 @@
 package librarymanager;
 
-public class LibraryManagementApp {
+
+import librarymanager.frontend.LibraryMainView;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import javax.swing.*;
+import java.awt.*;
+
+@SpringBootApplication
+public class LibraryManagementApp extends JFrame{
+
+    static LibraryMainView frame;
 
     public static void main(String[] args) {
 
+        ConfigurableApplicationContext context = createApplicationContext(args);
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    frame = new LibraryMainView();
+                    frame.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
         System.out.println("App started");
     }
+
+
+    private static ConfigurableApplicationContext createApplicationContext(String[] args) {
+
+        return new SpringApplicationBuilder(LibraryManagementApp.class)
+                .headless(false)
+                .run(args);
+    }
+
 }
